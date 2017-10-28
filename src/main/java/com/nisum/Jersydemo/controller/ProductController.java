363f2beeb28bf1b98105c68dba156eed76b0b7ed
sure harish <hsure@nisum.com>
2017-10-29 00:37:41 +0530

    Jersey Demo Project

---
+++ src/main/java/com/nisum/Jersydemo/controller/ProductController.java
@@ -0,0 +1,18 @@
+package com.nisum.Jersydemo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/product")
+public class ProductController {
+	@RequestMapping(value="/getAllProducts",method=RequestMethod.GET)
+	public ResponseEntity<?> getAllProducts()
+	{
+		return new ResponseEntity<>("hai", HttpStatus.OK);
+	}
+
+}
---
+++ src/test/java/com/nisum/Jersydemo/JersydemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nisum.Jersydemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JersydemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
