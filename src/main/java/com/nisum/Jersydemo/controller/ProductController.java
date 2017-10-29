9ee7eb6ad4f225d18b102e919522112d017125ef
sure harish <hsure@nisum.com>
2017-10-29 11:34:45 +0530

    created method for getting all the products

--- src/main/java/com/nisum/Jersydemo/controller/ProductController.java
+++ src/main/java/com/nisum/Jersydemo/controller/ProductController.java
@@ -1,18 +1,23 @@
 package com.nisum.Jersydemo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nisum.Jersydemo.service.ProductService;
+
 @RestController
 @RequestMapping(value="/product")
 public class ProductController {
+	@Autowired
+	ProductService productService;
 	@RequestMapping(value="/getAllProduct",method=RequestMethod.GET)
 	public ResponseEntity<?> getAllProducts()
 	{
-		return new ResponseEntity<>("hai", HttpStatus.OK);
+		return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK);
 	}
 
 }
---
+++ src/main/java/com/nisum/Jersydemo/service/ProductService.java
@@ -0,0 +1,15 @@
+package com.nisum.Jersydemo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+	
+	public String getAllProducts()
+	
+	{
+		return "all the prodcuts";
+	}
+	
+
+}
